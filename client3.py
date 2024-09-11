4753799b4bdd218d68483bdf88dd02b247b59f3c
Adunolami <abinatitilayo2@gmail.com>
2024-09-11 03:15:07 +0100

    Updated Client file

--- client3.py
+++ client3.py
@@ -35,14 +35,16 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return None
+    return price_a / price_b
 
 
 # Main
@@ -54,6 +56,14 @@ if __name__ == "__main__":
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            price[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            # Assuming the stock names are MNO AND PQR
+            price_a = price.get('MNO')
+            price_b = price.get('PQR')
 
-        print("Ratio %s" % getRatio(price, price))
+            ratio = getRatio(price_a, price_b)
+            if ratio is not None:
+                price(f"Ratio of MNO to PQR: {ratio}")
+            else:
+                print("Ratio %s" % getRatio(price, price))
